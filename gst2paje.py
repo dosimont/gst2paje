a220a283a1b01975c1ecf719719f64ebb0359ba4
dosimont <damien.dosimont@imag.fr>
2013-02-11 13:34:42 +0100

    bug corrected (wrong debuglevel name for function cont creating)

--- gst2paje.py
+++ gst2paje.py
@@ -117,7 +117,7 @@ for line in csvstreammod:
       container.append(Container(line[5] + "_on_" + line[2], "DebugLevel"))
       process[line[1]].thread[line[2]].debuglevel[line[5]] = DebugLevel(line[5])
     if not process[line[1]].thread[line[2]].debuglevel[line[5]].function.has_key(line[8]): 
-      ctypes.CDLL(libpoti).poti_CreateContainer(c_double(float(line[0])), line[8] + "_dbg_" + line[5] + "_on_" + line[2], "Function", line[5], line[8] + "_dbg_" + line[5] + "_on_" + line[2])
+      ctypes.CDLL(libpoti).poti_CreateContainer(c_double(float(line[0])), line[8] + "_dbg_" + line[5] + "_on_" + line[2], "Function", line[5] + "_on_" + line[2], line[8] + "_dbg_" + line[5] + "_on_" + line[2])
       container.append(Container(line[8] + "_on_" + line[2], "Function"))
       process[line[1]].thread[line[2]].debuglevel[line[5]].function[line[8]] = Function(line[8])
 #    if process[line[1]].thread[line[2]].currentfunction:
